92583097d8c2372dcaecd92442d95a24384967d3
kingg_sleyy <nonnystar53@gmail.com>
2019-10-25 15:41:38 -0300

    make it better

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,9 +2,10 @@ const bcrypt = require("bcrypt")
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
+const Collection = require('../models/collectionModel')
 
 exports.login = (req, res) => {
-    User.find({email: req.body.email })
+    User.find({ email: req.body.email })
         .exec()
         .then(user => {
             console.log(user)
@@ -23,19 +24,19 @@ exports.login = (req, res) => {
                     const token = jwt.sign({
                         email: user[0].email
                     },
-                    process.env.JWT_KEY,
+                        process.env.JWT_KEY,
                         {
                             expiresIn: "1h"
                         }
-                )
-                return res.status(200).json({
-                    message: "Auth successful",
-                    token: token,
-                    username: user[0].username,
-                    nationality: user[0].nationality,
-                    userId: user[0]._id
-                })
-            }
+                    )
+                    return res.status(200).json({
+                        message: "Auth successful",
+                        token: token,
+                        username: user[0].username,
+                        nationality: user[0].nationality,
+                        userId: user[0]._id
+                    })
+                }
                 else {
 
                     res.status(401).json({
@@ -43,12 +44,12 @@ exports.login = (req, res) => {
                     })
                 }
             })
-})
-        .catch (err => {
-    res.status(500).json({
-        error: err
-    })
-})
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 }
 exports.signup = (req, res) => {
     //validating request
@@ -104,31 +105,44 @@ exports.findAll = (req, res) => {
 
 exports.findOne = (req, res) => {
     console.log(req.params.userId)
-    User.findByIdAndUpdate(req.params.userId, req.body, {new: true}).
-    then(user => {
-        if (!user) {
-            return res.status(404).send({
-                message: "user not found" + req.params.userId
+    User.findByIdAndUpdate(req.params.userId, req.body, { new: true }).
+        then(user => {
+            if (!user) {
+                return res.status(404).send({
+                    message: "user not found" + req.params.userId
+                })
+            }
+            res.send(user);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "user not found with id" + req.params.userId
+                })
+            }
+            return res.status(500).send({
+                message: "Error retrieving user with id" + req.params.userId
             })
-        }
-        res.send(user);
-    }).catch(err => {
-        if (err.kind === 'ObjectId') {
-            return res.status(404).send({
-                message: "user not found with id" + req.params.userId
-            })
-        }
-        return res.status(500).send({
-            message: "Error retrieving user with id" + req.params.userId
         })
-    })
 
 }
 
 exports.deleteOne = (req, res) => {
-    User.findByIdAndRemove(req.params.userId).then(res => {
-        console.log(res)
+    User.findByIdAndRemove(req.params.userId).then(data => {
+
+        res.send(data)
+        if(data){                                                                                                                                                                                                                                                                                                                                                                               
+            Collection.findByIdAndRemove(req.params.userId).then(anotherData => {
+                res.send(anotherData)
+            }).catch(error => {
+                res.status(404).send({
+                    message: error.message
+                })
+            })
+        }
+    }).catch(error => {
+        res.status(404).send({
+            message: error.message
+        })
     })
 }
-
 
